8ef3112aa0fddef9c46b6391adb813fee7784349
Stean <stean.ngugi@student.moringaschool.com>
2025-05-08 21:08:16 +0300

    Final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_sequence = []
+    if length == 0:
+        print(fib_sequence)
+        return
+    elif length == 1:
+        fib_sequence = [0]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_num = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_num)
+    print(fib_sequence)
     pass
